2eb77aa43247a365b3f7b01c89c3e12ddc8c6be9
Peerachatdew <samr6yh9@gmail.com>
2025-12-10 17:17:02 +0700

    [GREEN] Implement sayHello to pass testHelloBasic

--- src/main/java/com/example/Greeting.java
+++ src/main/java/com/example/Greeting.java
@@ -2,9 +2,6 @@ package com.example;
 
 public class Greeting {
     public String sayHello(String name) {
-        if (name == null) {
-            return "Hello, Friend";
-        }
         return "Hello, " + name;
     }
 }
